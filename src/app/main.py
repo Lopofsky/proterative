b74c7f1890bfe6e3171f35766baed87005a504de
Lopofsky <sthrss@hotel-advance.com>
2020-12-21 16:37:36 +0200

    i) Introducing the 'forbidden' folder for templates that are not supposed to be accessed directly & ii) Check if the requested template/endpoint is supposed to be accessible.

--- src/app/main.py
+++ src/app/main.py
@@ -4,24 +4,25 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from os.path import dirname, basename, isfile, join, realpath
-from os import chdir, getcwd, name as os_name
+from os import chdir, getcwd, name as os_name, listdir
 from .db import form2DB, Database
 import glob
 import importlib
 
 fs, parent, DB, module_2_import = "\\" if os_name == 'nt' else '/', dirname(realpath(__file__)), Database(), "routers"
+templates_dir = parent+fs+"decoration"+fs+"templates"
+templates = Jinja2Templates(directory=templates_dir)
 
 app = FastAPI(debug=True)
+app.mount("/static/", StaticFiles(directory=parent+fs+"decoration"+fs+"static"), name="static")
 
 @app.on_event("startup")
 async def startup():
     await DB.connect()
     app.state.db = DB
 
-templates = Jinja2Templates(directory=parent+fs+"decoration"+fs+"templates")
-app.mount("/static/", StaticFiles(directory=parent+fs+"decoration"+fs+"static"), name="static")
-
-def load_all(module_2_import):
+@app.on_event("startup")
+async def load_all(module_2_import=module_2_import):
     if getcwd().find(fs+"app") == -1: chdir("app"+fs)
     if getcwd().find(module_2_import) == -1: chdir(module_2_import)
     modules = glob.glob(join(getcwd(), "*.py"))
@@ -31,21 +32,20 @@ def load_all(module_2_import):
     except Exception as e: raise e
     names = {(m.__name__, x):m for m in all_modules for x in m.__dict__ if not x.startswith("_")}
     globals_dict = {module_meta[0][len(module_2_import)+1:]+'_'+module_meta[1]: getattr(module_data, module_meta[1]) for module_meta, module_data in names.items()}
+    globals_dict['options'] = {str(x):y for x, y in globals_dict.items()}
+    globals_dict['html_templates'] = [f for f in listdir(templates_dir) if f.endswith(".html")]
     globals().update(globals_dict)
-    return {str(x):y for x, y in globals_dict.items()}
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/{Path_Param1}/{rest_of_path:path}", methods=["GET", "POST"])
 async def root(request: Request, Path_Param1: str='index', rest_of_path: str=''):
     # request.url.path ~~== request["path_params"]
-    path_exceptions = ['favicon.ico']
+    path_exceptions, err_page = ["forbidden"], "forbidden/403"
     payload = {"path":request.url.path}
     path_params = request["path_params"]
     if len(path_params) > 0: Path_Param1 = path_params['Path_Param1']
-    if Path_Param1 in path_exceptions: pass
-    else:
+    if Path_Param1 not in path_exceptions: 
         if Path_Param1 == '': Path_Param1 = 'index'
-        options = load_all(module_2_import)
         payload["page_requested"] = Path_Param1
         payload["form_data"] = {x[0]:x[1] for x in list((await request.form()).items())}
         payload["path_params"] = [x for x in path_params["rest_of_path"].split('/') if x is not None and x!=''] if "rest_of_path" in path_params else None
@@ -57,4 +57,6 @@ async def root(request: Request, Path_Param1: str='index', rest_of_path: str='')
         if renderer in options:
             select_func = (renderer, {"request":request, "payload":payload, "templates":templates})
             return await options[select_func[0].replace("'", "")](*select_func[1].values())
-        else: return templates.TemplateResponse(Path_Param1, {"request": request, "payload": payload})
+        elif Path_Param1 in html_templates: return templates.TemplateResponse(Path_Param1, {"request": request, "payload": payload})
+        else: err_page = "404"
+    return templates.TemplateResponse(err_page+".html", {"request": request})
