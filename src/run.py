9fa0cba8c818e5c7b7cd333c2532a3eaebb5765e
Lopofsky <sthrss@hotel-advance.com>
2021-07-08 21:40:08 +0300

    Specify imports @run.py + load 'EXTRA_DBS' environ to Docker environment.

--- src/run.py
+++ src/run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # # -*- coding: utf-8 -*-
 # SOURCE: https://github.com/encode/uvicorn/issues/706
-import uvicorn, platform
+from uvicorn import run
+from platform import system
 
 if __name__ == '__main__':
-    workers = 1 if platform.system() == 'Windows' else 4
-    async_loop = None if platform.system() == 'Windows' else 'uvloop'
-    if async_loop is None: uvicorn.run("app.main:app", host='0.0.0.0', reload=True, port=7000, forwarded_allow_ips="*", lifespan='on', workers=workers, interface='asgi3', timeout_keep_alive=5)
-    else: uvicorn.run("app.main:app", host='0.0.0.0', reload=True, port=7000, forwarded_allow_ips="*", lifespan='on', workers=workers, interface='asgi3', loop='uvloop', timeout_keep_alive=5)
+    workers = 1 if system() == 'Windows' else 4
+    async_loop = None if system() == 'Windows' else 'uvloop'
+    if async_loop is None: run("app.main:app", host='0.0.0.0', reload=True, port=7000, forwarded_allow_ips="*", lifespan='on', workers=workers, interface='asgi3', timeout_keep_alive=5)
+    else: run("app.main:app", host='0.0.0.0', reload=True, port=7000, forwarded_allow_ips="*", lifespan='on', workers=workers, interface='asgi3', loop='uvloop', timeout_keep_alive=5)
